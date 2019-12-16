2a35880a9390ddf68933844bc05bbf1bcbe3ac3d
Nurullah Akkaya <nurullah@nakkaya.com>
2019-12-16 23:07:56 +0200

    implement inf-ipy-* modes

--- inf-ipy.el
+++ inf-ipy.el
@@ -16,3 +16,22 @@
 
 (defun org-babel-execute:inf-ipy (body params)
   (comint-send-string "*inf-ipy*" (concat body "\n")))
+
+(defmacro inf-ipy-configure-kernel (kernel)
+  `(progn
+     (defvar ,(intern
+               (concat "org-babel-default-header-args:inf-ipy-"
+                       (symbol-name kernel)))
+       '((:results . "silent")))
+
+     (defun ,(intern
+              (concat "org-babel-execute:inf-ipy-"
+                      (symbol-name kernel)))
+         (body params)
+       (comint-send-string "*inf-ipy*" (concat body "\n")))
+
+     (add-to-list 'org-src-lang-modes
+                  '(,(concat "inf-ipy-" (symbol-name kernel)) . ,kernel))))
+
+;; (macroexpand '(inf-ipy-configure-kernel python))
+;; (inf-ipy-configure-kernel python)
