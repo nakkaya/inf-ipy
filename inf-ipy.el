83dba190d27abe2e0b062f3f5edd7e04f22a9606
Nurullah Akkaya <nurullah@nakkaya.com>
2019-12-16 21:48:25 +0200

    Update inf-ipy.el

--- inf-ipy.el
+++ inf-ipy.el
@@ -1,19 +1,12 @@
 (define-derived-mode inf-ipy-mode comint-mode "inf-ipy"
-  ;; This disables editing and traversing the "=>" prompts
   (setq-local comint-prompt-read-only t)
-  ;; Lets comint mode recognize the prompt
   (setq-local comint-prompt-regexp (rx bol ">" space)))
 
 (defun run-inf-ipy ()
   (interactive)
-  (let* ((buffer (comint-check-proc "inf-ipy")))
-    ;; pop to the "*inf-ipy*" buffer if the process is dead, the
-    ;; buffer is missing or it's got the wrong mode.
+  (let* ((buffer (comint-check-proc "*inf-ipy*")))
     (pop-to-buffer-same-window
-     (if (or buffer (comint-check-proc (current-buffer)))
-         (get-buffer-create (or buffer "*inf-ipy*"))
-       (current-buffer)))
-    ;; create the comint process if there is no buffer.
+     (get-buffer-create "*inf-ipy*"))
     (unless buffer
-      (apply 'make-comint "inf-ipy" "inf-ipy" nil '("--repl"))))
-  (inf-ipy-mode))
+      (apply 'make-comint "inf-ipy" "inf-ipy" nil '("--repl"))
+      (inf-ipy-mode))))
