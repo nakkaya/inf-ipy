82aa39d190fbe650cca42840b8247a29362ed5da
Nurullah Akkaya <nurullah@nakkaya.com>
2019-12-18 10:46:01 +0200

    fix setup py

--- src/python/setup.py
+++ src/python/setup.py
@@ -1,31 +1,29 @@
 import setuptools
 
-with open("readme.org", "r") as fh:
-    long_description = fh.read()
-    setuptools.setup(
-        name='inf_ipy',
-        version='0.1',
-        author="Nurullah Akkaya",
-        author_email="nurullah@nakkaya.com",
-        description="Manage Remote IPython Sessions",
+setuptools.setup(
+    name='inf_ipy',
+    version='0.1',
+    author="Nurullah Akkaya",
+    author_email="nurullah@nakkaya.com",
+    description="Manage Remote IPython Sessions",
 
         packages=['core'],
-        entry_points={
-            'console_scripts': [
-                'inf-ipy = core.__main__:main'
-            ]
-        },
-        install_requires=[
-            'paramiko',
-            'sshtunnel',
-            'jupyter_client',
-            'IPython',
-            'prompt_toolkit'
-        ],
-        
+    entry_points={
+        'console_scripts': [
+            'inf-ipy = core.__main__:main'
+        ]
+    },
+    install_requires=[
+        'paramiko',
+        'sshtunnel',
+        'jupyter_client',
+        'IPython',
+        'prompt_toolkit'
+    ],
+    
         classifiers=[
             "Programming Language :: Python :: 3",
             "License :: BSD 2-Clause",
             "Operating System :: OS Independent",
         ],
-    )
+)
